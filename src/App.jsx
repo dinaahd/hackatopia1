9aedc33e3cf1a6366469e60be942813bbc6759a0
dinaahd <dinaahd7@gmail.com>
2026-09-17 15:47:41 +0530

    new Error in App solved

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState, useRef, useCallback } from 'react'
+import { useEffect, useState, useRef } from 'react'
 import Lenis from 'lenis'
 import gsap from 'gsap'
 import { ScrollTrigger } from 'gsap/ScrollTrigger'
